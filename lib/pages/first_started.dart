97295fb4f79a292e29689e70f7dd6d8a6aacc4dd
BagusRezky <bagusrezky15@gmail.com>
2023-08-15 16:41:05 +0700

    Change Background

---
+++ lib/pages/first_started.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FirstStarted extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/cover.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
