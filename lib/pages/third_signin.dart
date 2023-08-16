70bad14535a262a4491a696a31bfb45ae14ecf16
BagusRezky <bagusrezky15@gmail.com>
2023-08-17 03:07:14 +0700

    Add TextFormField

--- lib/pages/third_signin.dart
+++ lib/pages/third_signin.dart
@@ -23,6 +23,52 @@ class SignIn extends StatelessWidget {
                   fontSize: 24,
                   fontWeight: FontWeight.w600),
             ),
+            SizedBox(height: 70),
+            TextFormField(
+              style: GoogleFonts.poppins(color: Colors.white),
+              decoration: InputDecoration(
+                fillColor: Color(0xff262A34),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(17),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Email',
+                hintStyle: GoogleFonts.openSans(
+                  color: Color(0xff6F7075),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              style: GoogleFonts.poppins(color: Colors.white),
+              decoration: InputDecoration(
+                fillColor: Color(0xff262A34),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(17),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Password',
+                hintStyle: GoogleFonts.openSans(
+                  color: Color(0xff6F7075),
+                ),
+                suffixIcon: Icon(
+                  Icons.visibility,
+                  color: Color(0xff6F7075),
+                ),
+              ),
+            ),
+            SizedBox(height: 6),
+            Container(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Forgot My Password',
+                style: GoogleFonts.poppins(
+                  color: Color(0xff6A6B70),
+                ),
+              ),
+            ),
           ],
         ),
       ),
