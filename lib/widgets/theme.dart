aad32a15f9707f376ac0f3acf54da77e68bc7f7f
BagusRezky <bagusrezky15@gmail.com>
2023-08-17 20:29:41 +0700

    Slicing With Theme

---
+++ lib/widgets/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = Color(0xff0E1954);
+TextStyle boldTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 15,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: Color(0xffF8F8F8),
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
