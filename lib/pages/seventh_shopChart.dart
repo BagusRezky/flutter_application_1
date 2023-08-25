267b5f06b74d5ee505d77a4fb22bcbc43c6c3ab8
BagusRezky <bagusrezky15@gmail.com>
2023-08-25 21:14:46 +0700

    Add Container and Heading

--- lib/pages/seventh_shopChart.dart
+++ lib/pages/seventh_shopChart.dart
@@ -5,6 +5,93 @@ import 'package:google_fonts/google_fonts.dart';
 class shopChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffFAFAFA),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 50.0,
+        ),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                'My Shopping Cart',
+                style: GoogleFonts.poppins(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff191919),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: 315,
+              height: 140,
+              decoration: BoxDecoration(
+                color: Color(0xffFFFFFF),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/burger.png',
+                        width: 80,
+                      ),
+                      SizedBox(height: 13),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/minus.png',
+                            width: 22,
+                          ),
+                          SizedBox(width: 8),
+                          Text('2'),
+                          SizedBox(width: 8),
+                          Image.asset(
+                            'assets/plus.png',
+                            width: 22,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Burger Malleta',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff191919),
+                        ),
+                      ),
+                      Text(
+                        'McTheone',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                          color: Color(0xffA3A8B8),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    '\$90.00',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xff191919),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
