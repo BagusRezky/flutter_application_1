f75ed491f136e15e08608d47e11db238b0872d8c
BagusRezky <bagusrezky15@gmail.com>
2023-10-22 21:03:45 +0700

    [Fix] Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/fifth_rating.dart';
-import 'package:flutter_application_1/pages/fourth_empty.dart';
-import 'package:flutter_application_1/pages/sixth_pricing.dart';
-import 'package:flutter_application_1/pages/third_signin.dart';
-import 'package:flutter_application_1/pages/first_splash.dart';
-import 'package:flutter_application_1/pages/first_started.dart';
 import 'package:flutter_application_1/pages/seventh_shopChart.dart';
 
-void main() => runApp(SevenDays());
+void main() => runApp(const SevenDays());
 
 class SevenDays extends StatelessWidget {
+  const SevenDays({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: pricing(),
+      home: ShopChart(),
     );
   }
 }
